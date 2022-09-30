42eb10e26e83bc21fde362e5ae78fa8ecb757a0c
amperezio <96073383+amperezio@users.noreply.github.com>
2022-09-30 16:02:38 -0400

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://music-front-bucket.s3-website-us-east-1.amazonaws.com/',
     credentials: true,
   });
 
